2b411b8994977777f2f24140c937ff14712d870d
aljagne <ajagne01@gmail.com>
2023-04-24 22:09:51 +0000

    Added my project

---
+++ src/Messsage.tsx
@@ -0,0 +1,10 @@
+//PascaleCasing
+function Message() {
+  //JSX: JavaScript XML
+  const name = "";
+  if (name) return <h1> Bonjour {name}</h1>;
+  return <h1>Bonjour La Monde </h1>;
+}
+
+
+export default Message;
